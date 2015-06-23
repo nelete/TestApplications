baa11adbdf2e7b776bd43fd8ab8df8f3c421404e
Nelo Torres <nelete@ono.com>
2015-06-23 18:39:08 +0200

    Recibiendo datos (Local)

--- app/src/main/java/greentoad/com/myapplication/MainActivity.java
+++ app/src/main/java/greentoad/com/myapplication/MainActivity.java
@@ -16,6 +16,7 @@ import android.widget.ListView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 
 import android.view.View;
@@ -23,8 +24,10 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -42,6 +45,8 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
     private EditText telefono=null;
     private EditText SIP=null;
 
+    final String LocalCall= "http://10.0.2.2/ToadService/GetUserByDNI.php?DNI=";
+    final String RemoteCall= "http://greentoad.esy.es/Prueba/ConsultarUsuario.php?DNI=";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -102,53 +107,14 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
 
     }
 
-public String httpGetData(String mURL) {
-        HttpURLConnection urlConnection=null;
-        InputStream stream = null;
-        try {
-
-            URL url = new URL(mURL);
-            urlConnection = (HttpURLConnection) url.openConnection();
-            try {
-                urlConnection.setRequestMethod("GET");
-                urlConnection.connect();
-
-                if (urlConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {
-                    stream = urlConnection.getInputStream();
-                }
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-            return("Hello");
-
-        }
-        catch (MalformedURLException ex) {
-            Log.e("httptest", Log.getStackTraceString(ex));
-            return(ex.getMessage());
-        }
-        catch (IOException ex) {
-            Log.e("httptest", Log.getStackTraceString(ex));
-            return(ex.getMessage());
-        }
-        finally {
-            try {
-                urlConnection.disconnect();
-            } catch (Exception e) {
-                e.printStackTrace(); //If you want further info on failure...
-            }
-        }
-    }
-
-
 
    //Métodos Privados
 
     private void InsertarUsuario(String numDNI,String nomb,String ape,String telef ,String numSIP){
         boolean Conected=NetworkIsConnected();
-        if (Conected==true) {
+        if (Conected) {
 
             httpSendDataTask Request=new httpSendDataTask();
-            httpSendDataTask.Result Res;
 
             Request.execute("http://10.0.2.2/prueba/registrarUsuario.php?DNI=" + numDNI +
                     "&Nombre=" + nomb + "&Apellido=" + ape +
@@ -194,39 +160,141 @@ public String httpGetData(String mURL) {
     }
 
     public void onFocusChange(View view,boolean hasFocus){
-
         if(hasFocus==false){
-            JSONArray ja=null;
-            try {
-                String data;
-                data=httpGetData("http://10.0.2.2/prueba/consultarUsuario.php?cc="+DNI.getText());
-                if(data.length()>1)
-                    ja=new JSONArray(data);
+
+            GetUserByDNI(DNI.getText().toString());
+
+        }
+
+    }
 
 
-            } catch (JSONException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-                Toast.makeText(getApplicationContext(), "Error recuperando la informacion del servidor, verifique su conexion a internet y vuelva a intentarlo.", Toast.LENGTH_LONG).show();
+    private void GetUserByDNI(String DNI) {
 
+        boolean Conected = NetworkIsConnected();
+        if (Conected ) {
+
+            httpGetDataTask Request = new httpGetDataTask();
+            httpGetDataTask.Result Res=null;
+            //http://localhost/ToadService/GetUserByDNI.php?DNI
+            //http://greentoad.esy.es/Prueba/ConsultarUsuario.php?DNI
+            Request.execute(RemoteCall + DNI);
+
+        }
+    }
+
+
+    private class httpGetDataTask extends AsyncTask<String,Void,httpGetDataTask.Result> {
+
+        @Override
+        protected Result doInBackground(String... url) {
+            Result output;
+            output = httpGetData(url[0]);
+            return output;
+        }
+
+        public class Result {
+
+            public JSONArray Response;
+            public int ResultCode;
+            public String Message;
+
+            public Result(JSONArray Resp, int code, String Mess) {
+                Response = Resp;
+                ResultCode = code;
+                Message = Mess;
             }
-            try{
-                nombre.setText(ja.getString(1));
-                apellido.setText(ja.getString(2));
-                telefono.setText(ja.getString(3));
-                SIP.setText(ja.getString(4));
-            } catch (Exception e) {
 
-                nombre.setText("");
-                apellido.setText("");
-                SIP.setText("");
-                telefono.setText("");
+        }
+
+        @Override
+        protected void onPostExecute(Result R) {
+            if (R.ResultCode == HttpURLConnection.HTTP_OK) {
+                if ( R!= null) {
+                    try {
+                        JSONObject obj=R.Response.getJSONObject(0);
+                        if (obj!=null) {
+
+                            nombre.setText(obj.getString("nombre"));
+                            apellido.setText(obj.getString("apellido1"));
+                            telefono.setText(obj.getString("DNI"));
+                            SIP.setText(obj.getString("permisos"));
+                        }
+
+                    } catch (JSONException e) {
+
+                        nombre.setText("");
+                        apellido.setText("");
+                        SIP.setText("");
+                        telefono.setText("");
+                    }
+                }
+                Toast.makeText(getApplicationContext(), "El dato ha sido traido correctamente", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "No se ha podido realizarla consulta", Toast.LENGTH_LONG).show();
             }
         }
 
 
+        private Result httpGetData(String mURL) {
+
+            HttpURLConnection urlConnection = null;
+            JSONArray ja = null;
+            Result Res=null;
+
+            try {
+
+                URL url = new URL(mURL);
+                urlConnection = (HttpURLConnection) url.openConnection();
+
+                try {
+                    urlConnection.setRequestMethod("GET");
+                    urlConnection.setDoOutput(true);
+                    urlConnection.connect();
+
+
+                    BufferedReader in = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+
+                    String line;
+
+
+                    while ((line = in.readLine()) != null) {
+                        try {
+                            ja = new JSONArray(line);
+                            Res= new Result(ja, HttpURLConnection.HTTP_OK, null);
+                        } catch (JSONException ex) {
+                            ex.printStackTrace();
+                            Toast.makeText(getApplicationContext(), "Error recuperando la respuesta del servidor.", Toast.LENGTH_LONG).show();
+                            Res= new Result(null, HttpURLConnection.HTTP_BAD_REQUEST, ex.getMessage());
+                        }
+                    }
+                }
+                //return new Result(urlConnection.getResponseCode(),"");
+                catch (Exception ex) {
+                    ex.printStackTrace();
+                    Toast.makeText(getApplicationContext(), "Error accediendo al servidor.", Toast.LENGTH_LONG).show();
+                    Res= new Result(null, HttpURLConnection.HTTP_BAD_REQUEST, ex.getMessage());
+                }
+
+            } catch (MalformedURLException ex) {
+                Log.e("httptest", Log.getStackTraceString(ex));
+                Res=new Result(null, HttpURLConnection.HTTP_NOT_ACCEPTABLE, ex.getMessage());
+                //return new Result(HttpURLConnection.HTTP_BAD_REQUEST,ex.getMessage());
+            } catch (IOException ex) {
+                Log.e("httptest", Log.getStackTraceString(ex));
+                Res= new Result(null, HttpURLConnection.HTTP_NOT_ACCEPTABLE, ex.getMessage());
+                //return new Result(HttpURLConnection.HTTP_FORBIDDEN,ex.getMessage());
+
+            } finally {
+
+                urlConnection.disconnect();
+                return Res;
+            }
+        }
 
     }
+
+
 
 
     private class httpSendDataTask extends AsyncTask<String, Void, httpSendDataTask.Result> {
@@ -295,9 +363,9 @@ public String httpGetData(String mURL) {
             }
         }
 
-
-
-
     }
+
+
+
 
 }
